e81b69dcb133e04ecce403cabc61ebbe822141fe
afedorov <andreyfedorovqq@gmail.com>
2021-07-20 00:48:38 +0300

    initial commit

---
+++ src/test/java/com/fedorov/springbootrest/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedorov.springbootrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
